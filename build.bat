6ba501c33343b381e50d7503f4c720df6460578c
Gabe Ambrosio <ambrosiogabe@gmail.com>
2020-09-01 14:30:55 -0500

    Update the submodules

--- build.bat
+++ build.bat
@@ -1,15 +1,3 @@
 @echo off
 
-if not exist JadeEditor\vendor\roslynVendor\artifacts\ (
-	echo "Building Roslyn library. This may take awhile."
-	echo "Restoring Roslyn library."
-	JadeEditor\vendor\roslynVendor\Restore.cmd
-	JadeEditor\vendor\roslynVendor\Build.cmd -Configuration Release -deployExtensions -launch
-)
-
-if not exist JadeEditor\vendor\roslynVendor\artifacts\bin\csc\Release\ (
-	echo "Building Roslyn library.
-	JadeEditor\vendor\roslynVendor\Build.cmd -Configuration Release -deployExtensions -launch
-)
-
 .\vendor\premake\premake5.exe vs2019
